739d89082c744035bfabf9d3e27b217f480977e0
Minghua Zhao <minghz42@gmail.com>
2018-05-28 22:09:34 -0400

    Remove CapsNet-Tensorflow from parent repo

---
+++ CapsNet-Tensorflow/README.md
@@ -0,0 +1,4 @@
+Taken from nautromics CapsNet-Tensorflow at commit:
+54c174e111aa4a6fe0f7044d9409edd9a125ad53
+
+Added custom modifications to run with affmnist dataset
---
+++ CapsNet-Tensorflow/utils.py
@@ -0,0 +1,228 @@
+import os
+import scipy
+import numpy as np
+import tensorflow as tf
+
+
+import scipy.io as spio
+from config import cfg
+from PIL import Image
+
+
+def loadmat(filename):
+    '''
+    this function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    '''
+    data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+
+def _check_keys(dict):
+    '''
+    checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries
+    '''
+    for key in dict:
+        if isinstance(dict[key], spio.matlab.mio5_params.mat_struct):
+            dict[key] = _todict(dict[key])
+    return dict
+
+
+def _todict(matobj):
+    '''
+    A recursive function which constructs from matobjects nested dictionaries
+    '''
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, spio.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+            dict[strg] = elem
+    return dict
+
+
+def load_mnist(batch_size, is_training=True):
+    path = os.path.join('data', 'mnist')
+    if is_training:
+        fd = open(os.path.join(path, 'train-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float32)
+
+        fd = open(os.path.join(path, 'train-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainY = loaded[8:].reshape((60000)).astype(np.int32)
+
+        trX = trainX[:55000] / 255.
+        trY = trainY[:55000]
+
+        valX = trainX[55000:, ] / 255.
+        valY = trainY[55000:]
+
+        num_tr_batch = 55000 // batch_size
+        num_val_batch = 5000 // batch_size
+
+        return trX, trY, num_tr_batch, valX, valY, num_val_batch
+    else:
+        fd = open(os.path.join(path, 't10k-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
+
+        fd = open(os.path.join(path, 't10k-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teY = loaded[8:].reshape((10000)).astype(np.int32)
+
+        num_te_batch = 10000 // batch_size
+        return teX / 255., teY, num_te_batch
+
+
+def load_fashion_mnist(batch_size, is_training=True):
+    path = os.path.join('data', 'fashion-mnist')
+    if is_training:
+        fd = open(os.path.join(path, 'train-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float32)
+
+        fd = open(os.path.join(path, 'train-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainY = loaded[8:].reshape((60000)).astype(np.int32)
+
+        trX = trainX[:55000] / 255.
+        trY = trainY[:55000]
+
+        valX = trainX[55000:, ] / 255.
+        valY = trainY[55000:]
+
+        num_tr_batch = 55000 // batch_size
+        num_val_batch = 5000 // batch_size
+
+        return trX, trY, num_tr_batch, valX, valY, num_val_batch
+    else:
+        fd = open(os.path.join(path, 't10k-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
+
+        fd = open(os.path.join(path, 't10k-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teY = loaded[8:].reshape((10000)).astype(np.int32)
+
+        num_te_batch = 10000 // batch_size
+        return teX / 255., teY, num_te_batch
+
+
+def load_affmnist(batch_size, is_training=True):
+    if is_training:
+        if cfg.peppered:
+            data_file = os.path.join(cfg.affmnist_data_dir,
+                                     'peppered_training_and_validation_batches',
+                                     cfg.peppered + '_percent.mat')
+        else:
+            data_file = os.path.join(cfg.affmnist_data_dir, 'just_centered', 'training_and_validation.mat')
+
+        images_per_transformation = int((60000 * int(cfg.peppered)/100) / 32)
+        num_inputs = images_per_transformation * 32 + 60000
+
+        data = loadmat(data_file)
+        images = data['affNISTdata']['image'].transpose().reshape(num_inputs, 40, 40, 1).astype(np.float32)
+        labels = data['affNISTdata']['label_int'].astype(np.uint8)
+        assert images.shape == (num_inputs, 40, 40, 1)
+        assert labels.shape == (num_inputs,)
+
+        default_num_training_inputs = num_inputs - 10000
+
+        trX = images[:default_num_training_inputs] / 255.
+        trY = labels[:default_num_training_inputs]
+
+        valX = images[10000:, ] / 255.
+        valY = labels[10000:]
+
+        num_tr_batch = 50000 // cfg.batch_size
+        num_val_batch = 10000 // cfg.batch_size
+
+        return trX, trY, num_tr_batch, valX, valY, num_val_batch
+
+    else:
+        # NOTE: Swap those two lines below to get some basic transformed test
+        data_file = os.path.join(cfg.affmnist_data_dir, 'transformed', 'test_batches', '15.mat')
+        #data_file = os.path.join(cfg.affmnist_data_dir, 'just_centered', 'test.mat')
+        data = loadmat(data_file)
+        images = data['affNISTdata']['image'].transpose().reshape(10000, 40, 40, 1).astype(np.float32)
+        labels = data['affNISTdata']['label_int'].astype(np.float32)
+        assert images.shape == (10000, 40, 40, 1)
+        assert labels.shape == (10000,)
+
+        imgs = images / 255.
+        labs = labels
+        num_te_batch = 10000 // cfg.batch_size
+
+        return imgs, labs, num_te_batch
+
+
+def load_data(dataset, batch_size, is_training=True, one_hot=False):
+    if dataset == 'mnist':
+        return load_mnist(batch_size, is_training)
+    elif dataset == 'fashion-mnist':
+        return load_fashion_mnist(batch_size, is_training)
+    elif dataset == 'affmnist':
+        return load_affmnist(batch_size, is_training)
+    else:
+        raise Exception('Invalid dataset, please check the name of dataset:', dataset)
+
+
+def get_batch_data(dataset, batch_size, num_threads):
+    if dataset == 'mnist':
+        trX, trY, num_tr_batch, valX, valY, num_val_batch = load_mnist(batch_size, is_training=True)
+    elif dataset == 'fashion-mnist':
+        trX, trY, num_tr_batch, valX, valY, num_val_batch = load_fashion_mnist(batch_size, is_training=True)
+    elif dataset == 'affmnist':
+        trX, trY, num_tr_batch, valX, valY, num_val_batch = load_affmnist(batch_size, is_training=True)
+
+    data_queues = tf.train.slice_input_producer([trX, trY])
+    X, Y = tf.train.shuffle_batch(data_queues, num_threads=num_threads,
+                                  batch_size=batch_size,
+                                  capacity=batch_size * 64,
+                                  min_after_dequeue=batch_size * 32,
+                                  allow_smaller_final_batch=False)
+
+    return(X, Y)
+
+
+def save_images(imgs, size, path):
+    '''
+    Args:
+        imgs: [batch_size, image_height, image_width]
+        size: a list with tow int elements, [image_height, image_width]
+        path: the path to save images
+    '''
+    imgs = (imgs + 1.) / 2  # inverse_transform
+    return(scipy.misc.imsave(path, mergeImgs(imgs, size)))
+
+
+def mergeImgs(images, size):
+    h, w = images.shape[1], images.shape[2]
+    imgs = np.zeros((h * size[0], w * size[1], 3))
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx // size[1]
+        imgs[j * h:j * h + h, i * w:i * w + w, :] = image
+
+    return imgs
+
+
+# For version compatibility
+def reduce_sum(input_tensor, axis=None, keepdims=False):
+    try:
+        return tf.reduce_sum(input_tensor, axis=axis, keepdims=keepdims)
+    except:
+        return tf.reduce_sum(input_tensor, axis=axis, keep_dims=keepdims)
+
+
+# For version compatibility
+def softmax(logits, axis=None):
+    try:
+        return tf.nn.softmax(logits, axis=axis)
+    except:
+        return tf.nn.softmax(logits, dim=axis)
