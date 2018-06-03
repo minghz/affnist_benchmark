0f25c43bb61d6ee5da4493e860e4b6b9a63a71fe
Minghua Zhao <minghz42@gmail.com>
2018-06-03 15:34:41 -0400

    Re-order configuration settings

--- lenet/config.py
+++ lenet/config.py
@@ -13,20 +13,19 @@ flags.DEFINE_integer('epoch', 50, 'epoch')
 ############################
 #   environment setting    #
 ############################
-flags.DEFINE_string('data_dir', '../affMNIST_data', 'Dir checkpoints are saved')
+flags.DEFINE_integer('num_threads', 8, 'number of threads of enqueueing exampls')
+
 flags.DEFINE_string('checkpoint_dir', 'checkpoint_dir', 'Dir checkpoints are saved')
-flags.DEFINE_integer('save_summaries_steps', 500, 'Number steps elapsed for summary save')
+flags.DEFINE_integer('save_checkpoint_steps', 1500, 'save checkpoint every #(steps)')
 
 flags.DEFINE_boolean('is_training', True, 'train or predict phase')
-flags.DEFINE_integer('num_threads', 8, 'number of threads of enqueueing exampls')
+flags.DEFINE_string('peppered', '70', 'affmnist peppered with transformed images of such percentage')
+flags.DEFINE_string('affmnist_data_dir', '../affMNIST_data', 'Dir for affmnist data')
+
 flags.DEFINE_string('logdir', 'logdir', 'logs directory')
+flags.DEFINE_integer('train_sum_freq', 1000, 'the frequency of saving train summary(step)')
+flags.DEFINE_integer('val_sum_freq', 1000, 'the frequency of saving valuation summary(step)')
 flags.DEFINE_string('results', 'results', 'path for saving results')
-flags.DEFINE_integer('train_sum_freq', 10, 'the frequency of saving train summary(step)')
-#flags.DEFINE_integer('train_sum_freq', 50, 'the frequency of saving train summary(step)')
-#flags.DEFINE_integer('train_sum_freq', 20, 'the frequency of saving train summary(step)')
-flags.DEFINE_integer('val_sum_freq', 10, 'the frequency of saving valuation summary(step)')
-#flags.DEFINE_integer('val_sum_freq', 1500, 'the frequency of saving valuation summary(step)')
-flags.DEFINE_integer('save_checkpoint_steps', 1500, 'save checkpoint every #(steps)')
 
 ############################
 #   distributed setting    #
